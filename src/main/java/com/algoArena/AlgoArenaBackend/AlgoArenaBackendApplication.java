73c64d70995fe6aa014913181fdf5a243e7cca2e
ravikant <ravikantyadav3243@gmail.com>
2025-12-25 11:11:36 +0530

    project setup

---
+++ src/main/java/com/algoArena/AlgoArenaBackend/AlgoArenaBackendApplication.java
@@ -0,0 +1,15 @@
+package com.algoArena.AlgoArenaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoArenaBackendApplication.class, args);
+	}
+       void run (){
+           System.out.println("application started");
+       }
+}
---
+++ src/test/java/com/algoArena/AlgoArenaBackend/AlgoArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoArena.AlgoArenaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
